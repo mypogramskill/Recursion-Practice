ee1c5af1a89f22388195fd635eb219431fa109a8
Muttayyab Abdurrehkman <muttayyab13@gmail.com>
2023-06-18 23:57:16 +0500

    Add files via upload

---
+++ Q1 print numbers.cpp
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+
+void number(int x);
+
+void number(int x)
+{
+	if(x>=1)
+	{
+		 number(x-1);
+		 printf("%d\n",x);
+	}
+
+}
+int main()
+{
+	int num;
+	printf("Enter Number");
+	scanf("%d",&num);
+	number(num);
+}
---
+++ Q2 sum of number.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include<stdio.h>
+int sum(int n);
+int main(){
+	
+	int num;
+	printf("Enter Numbers");
+	scanf("%d",&num);
+	printf("%d",sum(num));
+	
+}
+int sum(int n)
+{
+	if(n>=1)
+	return n+ sum(n-1);
+	else 
+	return n;
+}
